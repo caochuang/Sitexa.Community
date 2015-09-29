8bfa11d13eb7a841deec8a1beac977bf14f750c3
Oscar Peng <sitexa@gmail.com>
2015-09-29 15:55:06 +0800

    generate copyright for whole project

--- data/src/main/java/com/sitexa/android/data/net/okhttp/OkHttpApi.java
+++ data/src/main/java/com/sitexa/android/data/net/okhttp/OkHttpApi.java
@@ -1,3 +1,19 @@
+/*
+ *   Copyright (C) 2015 Sitexa Open Source Project
+ *   <p>
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   <p>
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   <p>
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package com.sitexa.android.data.net.okhttp;
 
 import android.content.Context;
@@ -122,9 +138,7 @@ public class OkHttpApi implements Callable<String> {
         if (isThereInternetConnection()) {
             try {
                 String json = okHttpClient.newCall(request).execute().body().string();
-
-                //if (json != null) throw new RuntimeException(TAG + ":json=" + json);
-
+                Log.d(TAG,json);
                 Map map = gson.fromJson(json, Map.class);
                 String code = (String) map.get(CodeConstants.HttpCode.CODE);
                 Object value = map.get(CodeConstants.HttpCode.VALUE);
@@ -153,7 +167,6 @@ public class OkHttpApi implements Callable<String> {
         if (isThereInternetConnection()) {
             try {
                 String json = okHttpClient.newCall(request).execute().body().string();
-                //if (json != null) throw new RuntimeException(TAG + ":" + json);
                 Log.d(TAG,json);
                 Map map = gson.fromJson(json, Map.class);
                 String code = (String) map.get(CodeConstants.HttpCode.CODE);
--- data/src/main/java/com/sitexa/android/data/net/okhttp/UserApi.java
+++ data/src/main/java/com/sitexa/android/data/net/okhttp/UserApi.java
@@ -1,17 +1,17 @@
-/**
- * Copyright (C) 2015 Fernando Cejas Open Source Project
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ *   Copyright (C) 2015 Sitexa Open Source Project
+ *   <p>
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   <p>
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   <p>
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package com.sitexa.android.data.net.okhttp;
 
@@ -25,14 +25,10 @@ import rx.Observable;
  * RestApi for retrieving data from the network.
  */
 public interface UserApi {
-    //static final String API_BASE_URL = "http://www.android10.org/myapi/";
-
     /** Api url for getting all users */
-    //static final String API_URL_GET_USER_LIST = API_BASE_URL + "users.json";
     String API_URL_GET_USER_LIST = "/user/getUserList";
 
-    /** Api url for getting a user profile: Remember to concatenate id + 'json' */
-    //static final String API_URL_GET_USER_DETAILS = API_BASE_URL + "user_";
+    /** Api url for getting a user profile */
     String API_URL_GET_USER_DETAILS = "/user/findUserById";
 
     /**
